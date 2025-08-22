3226fe2dafa8ca8a26ecc70aa0236da66d0efc6d
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-22 11:55:28 +0100

    backend code commit by ENGR. IPAYE

--- java-backend/src/main/java/org/engripaye/javabackend/repository/ExpenseRepo.java
+++ java-backend/src/main/java/org/engripaye/javabackend/repository/ExpenseRepo.java
@@ -1,4 +1,10 @@
 package org.engripaye.javabackend.repository;
 
-public interface ExpenseRepo {
+import org.engripaye.javabackend.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExpenseRepo extends JpaRepository<Expense, Long> {
+    List<Expense> findByCategory(String category);
 }
